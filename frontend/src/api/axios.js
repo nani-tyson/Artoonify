1a973c9f879f9b4fd3a3fdb80799a96e6eab2088
nani-tyson <nanityson10@gmail.com>
2025-09-02 19:35:43 +0530

    Replaced deployed backend url in axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',  // Adjust the backend URL if necessary
+  baseURL: ' https://artoonify.onrender.com',  // Adjust the backend URL if necessary
 });
 
 export default instance;
